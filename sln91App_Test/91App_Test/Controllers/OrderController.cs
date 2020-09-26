37213b1fecd28ae5c4b93ee37284c62cf09d7840
CGW1996 <s22760081@gmail.com>
2020-09-26 15:53:01 +0800

    MVC_code

---
+++ sln91App_Test/91App_Test/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _91App_Test.Controllers
+{
+    public class OrderController : Controller
+    {
+        dbOrderListEntities db = new dbOrderListEntities();
+        public ActionResult Index()
+        {
+            var orderList = from t in db.tOrderList
+                            select t;
+            return View(orderList);
+        }
+        public ActionResult changeStatus(int[] chkVal)
+        {
+            foreach (var i in chkVal)
+            {
+                tOrderList t = db.tOrderList.FirstOrDefault(x => x.forderListId == i);
+                t.fOrderStatus = 2;
+            }
+            db.SaveChanges();
+            var z = from y in db.tOrderList
+                    select y;
+            return Json(z);
+        }
+        public ActionResult detail(int? Id)
+        {
+            tOrderList t = db.tOrderList.FirstOrDefault(x => x.forderListId == Id);
+            return View(t);
+        }
+    }
+}
---
+++ sln91App_Test/91App_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_91App_Test.MvcApplication" Language="C#" %>
